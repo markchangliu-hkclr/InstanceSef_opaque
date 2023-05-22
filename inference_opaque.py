f6ac51150a0696f2af58bb8da0809d2365ad5efa
Liu Chang <cliu@hkclr.hk>
2023-05-22 18:32:12 +0800

    update inference_opaque.py

--- inference_opaque.py
+++ inference_opaque.py
@@ -1,74 +1,50 @@
-from mmdet.apis import init_detector, inference_detector
-import mmcv
 import os
-import os.path as osp
 import time
-import numpy as np
 import cv2
-import torch
-from matplotlib import pyplot as plt
+import numpy as np
+from pathlib import Path
+from mmdet.apis import init_detector, inference_detector
+from autostore.core.logging import get_logger
+from autostore.core.io import load_img_paths
 
 
-config_file = './configs/mask_rcnn_r50_caffe_fpn_mstrain-poly_3x_coco_opaque.py'
-# config_file = './configs/mask_rcnn_r50_caffe_fpn_detectron2_400ep_LSJ_opaque.py'
-# config_file = './configs/cascade_mask_rcnn_r50_caffe_fpn_mstrain_3x_coco_opaque.py'
-# config_file = './configs/mask_rcnn_r101_caffe_fpn_mstrain-poly_3x_coco_opaque.py'
-time_start = time.time()
-checkpoint_name = 'epoch_2_small'
-# checkpoint_name = 'epoch_2_2303241551_LSJ'
-checkpoint_file  = 'exps_opaque0/' + checkpoint_name +'.pth'
+logger = get_logger(__name__)
 
 
-# build the model from a config file and a checkpoint file
-model = init_detector(config_file, checkpoint_file, device='cuda:0')
-print("Time consumed model load: ", time.time()-time_start)
-
-# x_min, x_max, y_min, y_max = 430, 1000, 170, 560
-# x_min, x_max, y_min, y_max = 460, 930, 190, 530
-# x_min, x_max, y_min, y_max = 410, 1010, 110, 560
-# x_min, x_max, y_min, y_max = 590, 890, 150, 380 # photoneo autostore
-x_min, x_max, y_min, y_max = 395, 845, 175, 525 # photoneo autostore v2
-# x_min, x_max, y_min, y_max = 560, 910, 80, 340 # AutoStore black bin
-# x_min, x_max, y_min, y_max = 545, 955, 75, 345 # photoneo poc
-# x_min, x_max, y_min, y_max = 0, 1280, 0, 720 # cropped images
-# x_min, x_max, y_min, y_max = 580, 905, 85, 325 # Test case
-
-# test a single image and show the results
-# folder = '/home/xjgao/Dataset/Autostore/Labeled_Autostore_all/test'
-# folder = '/home/xjgao/Dataset/Autostore/Autostore_230217'
-# folder = '/home/xjgao/Dataset/Autostore/Bin_det'
-# folder = '/home/xjgao/Dataset/Autostore/Failure_case'
-folder = "/home/xjgao/Dataset/Autostore/Labeled_Autostore_all/test"
-
-
-# sub_folder_list = os.listdir(folder)
-sub_folder_list = ['1']
-
-for id in sub_folder_list:
-    img_dir = os.path.join(folder, id)
-
-    for img_name in os.listdir(img_dir):
-        if img_name.split(".")[-1]!="png" and img_name.split(".")[-1]!="jpg":
-            continue
-
-        img_path = osp.join(img_dir, img_name)
-        print(img_path)
+def model_inference(
+        img_dir: str,
+        mmdet_cfg: str,
+        weight_pth: str,
+        show_score_thr: int,
+        report_dir: str,
+    ) -> None:
+    model = init_detector(mmdet_cfg, weight_pth, device='cuda:0')
+    time_deltas = []
+    for img_path, img_name, img_ext in load_img_paths(img_dir):
         img = cv2.imread(img_path)
+        t1 = time.time()
+        result = inference_detector(model, img)
+        t2 = time.time()
+        time_deltas.append(t2 - t1)
+        if report_dir:
+            os.makedirs(Path(report_dir), exist_ok=True)
+            visual_save_path = os.path.join(report_dir, f"{img_name}_inf.{img_ext}")
+            model.show_result(
+                img_path, 
+                result, 
+                score_thr=show_score_thr, 
+                out_file=visual_save_path, 
+                font_size=9
+            )
+    time_deltas = np.array(time_deltas)
+    logger.info(f"avg time consumed for inferencing \
+                {len(time_deltas)} images is {time_deltas.mean()}")
+    
 
-        img_height, img_weight = img.shape[0], img.shape[1]
-        cropped_image = img[y_min:y_max, x_min:x_max]
-        time_start = time.time()
-        result = inference_detector(model, cropped_image)
-        print("Time consumed: ", time.time()-time_start)
-        # model.show_result(cropped_image, result, out_file=osp.join(img_dir, 'seg_results', img_name.split('.')[0]+'_crop_result.png'))
-        model.show_result(cropped_image, result, out_file=osp.join('./tmp', img_name.split('.')[0]+'_crop_result.png'))
-        bboxes_, masks_ = result
-        masks_ = np.asarray(masks_[0])
-        masks = np.zeros((masks_.shape[0], img_height, img_weight))
-        if len(masks)==0: continue
-        masks[:, y_min:y_max, x_min:x_max] = masks_
-        np.savez_compressed(os.path.join(img_dir, 'masks_' + img_name.split('.')[0]), masks)
-
-        ## save cropped image
-        # plt.imsave(os.path.join(img_dir, 'seg_results', img_name.split('.')[0]+'_crop.png'), img[y_min:y_max, x_min:x_max, ::-1])
-        # plt.imsave(os.path.join('./tmp', img_name.split('.')[0]+'_crop.png'), img[y_min:y_max, x_min:x_max, ::-1])
+if __name__ == "__main__":
+    img_dir = "/home/cliu/InstanceSef_opaque/data/commodity_group_rsz"
+    mmdet_cfg = "/home/cliu/InstanceSef_opaque/configs/mmdet_commit/mask_rcnn_r50_caffe_fpn_detectron2_400ep_LSJ_opaque.py"
+    weight_pth = "/home/cliu/InstanceSef_opaque/exps/exps_opaque1/epoch_2_small.pth"
+    show_score_thr = 0.8
+    report_dir = "./exps/exps_opaque1_rsz"
+    model_inference(img_dir, mmdet_cfg, weight_pth, show_score_thr, report_dir)
